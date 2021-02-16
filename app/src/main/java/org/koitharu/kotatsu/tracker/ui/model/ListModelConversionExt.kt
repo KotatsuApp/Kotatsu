d9d5595bdeca4bd5bd2bb59c87a93b6ae1f9ba5d
Koitharu <nvasya95@gmail.com>
2021-02-16 20:03:25 +0200

    Ellipsize chapters in feed

--- app/src/main/java/org/koitharu/kotatsu/tracker/ui/model/ListModelConversionExt.kt
+++ app/src/main/java/org/koitharu/kotatsu/tracker/ui/model/ListModelConversionExt.kt
@@ -6,21 +6,37 @@ import org.koitharu.kotatsu.R
 import org.koitharu.kotatsu.core.model.TrackingLogItem
 import org.koitharu.kotatsu.utils.ext.formatRelative
 
-fun TrackingLogItem.toFeedItem(resources: Resources) = FeedItem(
-	id = id,
-	imageUrl = manga.coverUrl,
-	title = manga.title,
-	subtitle = buildString {
-		append(createdAt.formatRelative(DateUtils.DAY_IN_MILLIS))
-		append(" ")
-		append(
-			resources.getQuantityString(
-				R.plurals.new_chapters,
-				chapters.size,
-				chapters.size
+fun TrackingLogItem.toFeedItem(resources: Resources): FeedItem {
+	val chaptersString = if (chapters.size > MAX_CHAPTERS) {
+		chapters.joinToString(
+			separator = "\n",
+			limit = MAX_CHAPTERS - 1,
+			truncated = resources.getString(
+				R.string._and_x_more,
+				chapters.size - MAX_CHAPTERS + 1
 			)
 		)
-	},
-	chapters = chapters.joinToString("\n"),
-	manga = manga
-)
+	} else {
+		chapters.joinToString("\n")
+	}
+	return FeedItem(
+		id = id,
+		imageUrl = manga.coverUrl,
+		title = manga.title,
+		subtitle = buildString {
+			append(createdAt.formatRelative(DateUtils.DAY_IN_MILLIS))
+			append(" ")
+			append(
+				resources.getQuantityString(
+					R.plurals.new_chapters,
+					chapters.size,
+					chapters.size
+				)
+			)
+		},
+		chapters = chaptersString,
+		manga = manga
+	)
+}
+
+private const val MAX_CHAPTERS = 6
