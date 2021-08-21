52fded3e73b3c7c2d91a80684ec25883e31c902d
Zakhar Timoshenko <vp1984tanki@gmail.com>
2021-08-21 20:25:43 +0300

    Fix app crash on the first use

--- app/src/main/java/org/koitharu/kotatsu/core/db/DatabasePrePopulateCallback.kt
+++ app/src/main/java/org/koitharu/kotatsu/core/db/DatabasePrePopulateCallback.kt
@@ -4,13 +4,14 @@ import android.content.res.Resources
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import org.koitharu.kotatsu.R
+import org.koitharu.kotatsu.core.model.SortOrder
 
 class DatabasePrePopulateCallback(private val resources: Resources) : RoomDatabase.Callback() {
 
 	override fun onCreate(db: SupportSQLiteDatabase) {
 		db.execSQL(
-			"INSERT INTO favourite_categories (created_at, sort_key, title) VALUES (?,?,?)",
-			arrayOf(System.currentTimeMillis(), 1, resources.getString(R.string.read_later))
+			"INSERT INTO favourite_categories (created_at, sort_key, title, `order`) VALUES (?,?,?,?)",
+			arrayOf(System.currentTimeMillis(), 1, resources.getString(R.string.read_later), SortOrder.NEWEST.name)
 		)
 	}
 }
