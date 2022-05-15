5abf5d3367a574c2116c28bb5f580ef9772b74e4
Koitharu <nvasya95@gmail.com>
2022-05-15 16:36:08 +0300

    Update gradle and app version

--- app/src/test/java/org/koitharu/kotatsu/core/github/VersionIdTest.kt
+++ app/src/test/java/org/koitharu/kotatsu/core/github/VersionIdTest.kt
@@ -1,50 +1,52 @@
 package org.koitharu.kotatsu.core.github
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.koitharu.kotatsu.BuildConfig
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class VersionIdTest {
 
 	@Test
 	fun testVersionIdParse() {
-		val version = VersionId.parse("2.0")
+		val version = VersionId("2.0")
 		assertEquals(version.major, 2)
-		assertEquals(version.minor,0)
-		assertEquals(version.build,0)
+		assertEquals(version.minor, 0)
+		assertEquals(version.build, 0)
 		assertEquals(version.variantType, "")
-		assertEquals(version.variantNumber,0)
+		assertEquals(version.variantNumber, 0)
 	}
 
 	@Test
 	fun testVersionIdVariantParse() {
-		val version = VersionId.parse("2.0.1-b1")
+		val version = VersionId("2.0.1-b1")
 		assertEquals(version.major, 2)
 		assertEquals(version.minor, 0)
 		assertEquals(version.build, 1)
 		assertEquals(version.variantType, "b")
 		assertEquals(version.variantNumber, 1)
+		val version2 = VersionId("2.0.1-beta1")
+		assertEquals(compareValues(version, version2), 0)
 	}
 
 	@Test
 	fun testVersionIdCompare() {
-		val version1 = VersionId.parse("1.99.99")
-		val version2 = VersionId.parse("2.0.0")
+		val version1 = VersionId("1.99.99")
+		val version2 = VersionId("2.0.0")
 		assertTrue(version1 < version2)
 	}
 	
 	@Test
 	fun testVersionIdVariantCompare() {
-		val version1 = VersionId.parse("2.0.1-a2")
-		val version2 = VersionId.parse("2.0.1-b1")
+		val version1 = VersionId("2.0.1-a2")
+		val version2 = VersionId("2.0.1-b1")
 		assertTrue(version1 < version2)
 	}
 
 	@Test
 	fun testCurrentVersion() {
-		val version1 = VersionId.parse("2.4.6")
-		val version2 = VersionId.parse(BuildConfig.VERSION_NAME)
+		val version1 = VersionId("2.4.6")
+		val version2 = VersionId(BuildConfig.VERSION_NAME)
 		assertTrue(version1 < version2)
 	}
 }
