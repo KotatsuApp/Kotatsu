96bac81b8459ad7d213dc61a7f8f498ee5835144
Koitharu <nvasya95@gmail.com>
2023-08-11 15:13:25 +0300

    Fix captcha detection

--- app/src/main/kotlin/org/koitharu/kotatsu/core/network/CloudFlareInterceptor.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/network/CloudFlareInterceptor.kt
@@ -3,20 +3,21 @@ package org.koitharu.kotatsu.core.network
 import okhttp3.Interceptor
 import okhttp3.Response
 import okhttp3.internal.closeQuietly
+import org.jsoup.Jsoup
 import org.koitharu.kotatsu.core.exceptions.CloudFlareProtectedException
 import org.koitharu.kotatsu.parsers.model.MangaSource
 import java.net.HttpURLConnection.HTTP_FORBIDDEN
 import java.net.HttpURLConnection.HTTP_UNAVAILABLE
-
-private const val HEADER_SERVER = "Server"
-private const val SERVER_CLOUDFLARE = "cloudflare"
 
 class CloudFlareInterceptor : Interceptor {
 
 	override fun intercept(chain: Interceptor.Chain): Response {
 		val response = chain.proceed(chain.request())
 		if (response.code == HTTP_FORBIDDEN || response.code == HTTP_UNAVAILABLE) {
-			if (response.header(HEADER_SERVER)?.startsWith(SERVER_CLOUDFLARE) == true) {
+			val content = response.body?.source()?.peek()?.use {
+				Jsoup.parse(it.inputStream(), Charsets.UTF_8.name(), response.request.url.toString())
+			} ?: return response
+			if (content.getElementById("challenge-error-title") != null) {
 				val request = response.request
 				response.closeQuietly()
 				throw CloudFlareProtectedException(
