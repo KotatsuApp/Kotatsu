d918b1e27497231a50140b483f5ea8afef028345
Koitharu <nvasya95@gmail.com>
2025-03-02 13:35:09 +0200

    Fix database migration

--- app/src/main/kotlin/org/koitharu/kotatsu/core/db/migrations/Migration24To25.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/db/migrations/Migration24To25.kt
@@ -7,6 +7,6 @@ class Migration24To25 : Migration(24, 25) {
 
 	override fun migrate(db: SupportSQLiteDatabase) {
 		db.execSQL("ALTER TABLE manga ADD COLUMN content_rating TEXT DEFAULT NULL")
-		db.execSQL("UPDATE manga SET content_rating = (SELECT IIF(m.nsfw, 'ADULT', NULL) FROM manga AS m WHERE manga.manga_id = m.manga_id)")
+		db.execSQL("UPDATE manga SET content_rating = 'ADULT' WHERE nsfw = 1")
 	}
 }
