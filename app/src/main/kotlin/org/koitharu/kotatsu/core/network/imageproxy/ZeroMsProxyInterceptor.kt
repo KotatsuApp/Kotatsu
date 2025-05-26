f9f16d4a57fdd9738ab1d75aa3380b602eaad612
arasseo. <34034590+galpt@users.noreply.github.com>
2025-05-26 15:33:34 +0700

    only update domain

--- app/src/main/kotlin/org/koitharu/kotatsu/core/network/imageproxy/ZeroMsProxyInterceptor.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/network/imageproxy/ZeroMsProxyInterceptor.kt
@@ -8,17 +8,17 @@ import okhttp3.Request
 class ZeroMsProxyInterceptor : BaseImageProxyInterceptor() {
 
 	override suspend fun onInterceptImageRequest(request: ImageRequest, url: HttpUrl): ImageRequest {
-		if (url.host == "x.0ms.dev" || url.host == "0ms.dev") {
+		if (url.host == "v.recipes") {
 			return request
 		}
-		val newUrl = ("https://x.0ms.dev/q70/$url").toHttpUrl()
+		val newUrl = ("https://v.recipes/i/$url").toHttpUrl()
 		return request.newBuilder()
 			.data(newUrl)
 			.build()
 	}
 
 	override suspend fun onInterceptPageRequest(request: Request): Request {
-		val newUrl = ("https://x.0ms.dev/q70/${request.url}").toHttpUrl()
+		val newUrl = ("https://v.recipes/i/${request.url}").toHttpUrl()
 		return request.newBuilder()
 			.url(newUrl)
 			.build()
