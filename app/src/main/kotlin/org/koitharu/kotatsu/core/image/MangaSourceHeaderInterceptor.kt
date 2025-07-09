b528b7b3c18268ceb66c8635cdf64871b7d4b0dd
Koitharu <nvasya95@gmail.com>
2025-07-09 22:07:37 +0300

    Fix passing headers to favicon requests

--- app/src/main/kotlin/org/koitharu/kotatsu/core/image/MangaSourceHeaderInterceptor.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/image/MangaSourceHeaderInterceptor.kt
@@ -3,6 +3,7 @@ package org.koitharu.kotatsu.core.image
 import coil3.intercept.Interceptor
 import coil3.network.httpHeaders
 import coil3.request.ImageResult
+import org.koitharu.kotatsu.core.model.unwrap
 import org.koitharu.kotatsu.core.network.CommonHeaders
 import org.koitharu.kotatsu.core.util.ext.mangaSourceKey
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
@@ -10,7 +11,7 @@ import org.koitharu.kotatsu.parsers.model.MangaParserSource
 class MangaSourceHeaderInterceptor : Interceptor {
 
 	override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
-		val mangaSource = chain.request.extras[mangaSourceKey] as? MangaParserSource ?: return chain.proceed()
+		val mangaSource = chain.request.extras[mangaSourceKey]?.unwrap() as? MangaParserSource ?: return chain.proceed()
 		val request = chain.request
 		val newHeaders = request.httpHeaders.newBuilder()
 			.set(CommonHeaders.MANGA_SOURCE, mangaSource.name)
