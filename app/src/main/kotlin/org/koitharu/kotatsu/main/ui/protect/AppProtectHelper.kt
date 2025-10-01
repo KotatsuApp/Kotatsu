9ce9c039082d36d5a5f63ff037470e4da321aef2
vtorres-t <73935362+vtorres-t@users.noreply.github.com>
2025-10-02 00:35:37 +0200

    Update AppProtectHelper.kt

--- app/src/main/kotlin/org/koitharu/kotatsu/main/ui/protect/AppProtectHelper.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/main/ui/protect/AppProtectHelper.kt
@@ -3,7 +3,6 @@ package org.koitharu.kotatsu.main.ui.protect
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import org.acra.dialog.CrashReportDialog
 import org.koitharu.kotatsu.core.prefs.AppSettings
 import org.koitharu.kotatsu.core.ui.DefaultActivityLifecycleCallbacks
 import javax.inject.Inject
