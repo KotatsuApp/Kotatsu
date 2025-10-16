910be578bcbf279bf509de9aeb183ce6eb1a9aa5
Nathan Bapin <bapin.nathan@gmail.com>
2025-10-16 11:42:25 +0200

    fix error message when opening a chapter

--- app/src/main/kotlin/org/koitharu/kotatsu/reader/ui/pager/BaseReaderFragment.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/reader/ui/pager/BaseReaderFragment.kt
@@ -30,8 +30,18 @@ abstract class BaseReaderFragment<B : ViewBinding> : BaseFragment<B>(), ZoomCont
 			// - getCurrentState(): current reading position saved in SavedStateHandle
 			val currentState = viewModel.getCurrentState()
 			val pendingState = when {
-				it.state == null && it.pages.isNotEmpty() && readerAdapter?.hasItems != true -> currentState
-				readerAdapter?.hasItems != true && it.state != currentState && currentState != null -> currentState
+				// If content.state is null and we have pages, use getCurrentState
+				it.state == null
+					&& it.pages.isNotEmpty()
+					&& readerAdapter?.hasItems != true -> currentState
+
+				// use currentState only if it matches the current pages (to avoid the error message)
+				readerAdapter?.hasItems != true
+					&& it.state != currentState
+					&& currentState != null
+					&& it.pages.any { page -> page.chapterId == currentState.chapterId } -> currentState
+
+				// Otherwise, use content.state (normal flow, mode switch, chapter change)
 				else -> it.state
 			}
 			onPagesChanged(it.pages, pendingState)
